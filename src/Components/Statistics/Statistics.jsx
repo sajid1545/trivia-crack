27a902108abf13af522a2376fbf693f15a8d24d2
sajid9962 <clashking1545@gmail.com>
2022-10-11 01:37:34 +0600

    charts added in statistics component

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TopicContext } from '../../Layout/Main';
+import {
+	BarChart,
+	Bar,
+	Cell,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+	LineChart,
+	Line,
+	PieChart,
+	Pie,
+} from 'recharts';
 
 const Statistics = () => {
-    return (
+	const topics = useContext(TopicContext);
+
+	return (
         <div>
-            
-        </div>
-    );
+            <h1 className='text-5xl text-center font-bold my-5 text-purple-800'>Statistics</h1>
+			<div className="grid grid-cols-1 lg:grid-cols-2 max-w-[1300px] mx-auto gap-5 ">
+				<ResponsiveContainer width={'99%'} height={300} className="mt-10">
+					<BarChart width={150} height={40} data={topics}>
+						<Tooltip />
+						<XAxis dataKey="name" />
+						<Bar dataKey="total" fill="#A91079" />
+						<Legend />
+					</BarChart>
+				</ResponsiveContainer>
+
+				<ResponsiveContainer width={'99%'} height={300} className="mt-10">
+					<LineChart width={300} height={100} data={topics}>
+						<Line type="monotone" dataKey="total" stroke="#FF731D" strokeWidth={2} />
+						<XAxis dataKey="name" />
+						<Legend />
+						<Tooltip />
+					</LineChart>
+				</ResponsiveContainer>
+			</div>
+		</div>
+	);
 };
 
 export default Statistics;
