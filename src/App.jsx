a004579441c92ea4d20ba6ae9180f75a70727933
sajid9962 <clashking1545@gmail.com>
2022-10-10 20:52:28 +0600

    NavBar component created, and path is set!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Router/router';
 
 function App() {
 
   return (
     <div >
-     
+     <RouterProvider router={router}/>
     </div>
   )
 }
