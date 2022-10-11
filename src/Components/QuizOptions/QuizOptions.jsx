2c5ef5675ae5f6cba081af987d2dbc8c6ac8ed00
sajid9962 <clashking1545@gmail.com>
2022-10-11 20:03:40 +0600

    radio button fixed

--- src/Components/QuizOptions/QuizOptions.jsx
+++ src/Components/QuizOptions/QuizOptions.jsx
@@ -3,20 +3,16 @@ import React from 'react';
 const QuizOptions = ({ option, handleAnswer }) => {
 	return (
 		<div>
-			<div
-				
-				className="bg-white p-5 rounded-xl font-bold cursor-pointer duration-500 hover:bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900 hover:text-white ">
-				<label  className="label">
-					<div onClick={() => handleAnswer(option)} className="flex gap-4 items-center  ">
-						<input
-							type="radio"
-							name="radio-3"
-							className="radio radio-primary  hidden sm:inline-block"
-						/>
-						<span>{option}</span>
-					</div>
-				</label>
-			</div>
+			<label className="label cursor-pointer bg-white p-5 rounded-xl font-bold  duration-500 hover:bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900 hover:text-white">
+				<div onClick={() => handleAnswer(option)} className="flex gap-4 items-center   ">
+					<input
+						type="radio"
+						name="radio-3"
+						className="radio radio-primary  hidden sm:inline-block"
+					/>
+					<span>{option}</span>
+				</div>
+			</label>
 		</div>
 	);
 };
